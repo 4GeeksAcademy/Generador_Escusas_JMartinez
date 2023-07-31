10ccc8a291c688369614a64b961536e3fe5cbb1f
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-07-31 14:29:45 +0000

    entrega

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function cambio() {
+window.onload = function() {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -21,21 +21,10 @@ window.onload = function cambio() {
   let action1 = Math.floor(Math.random() * (4 - 0) + 0);
   let what1 = Math.floor(Math.random() * (3 - 0) + 0);
   let when1 = Math.floor(Math.random() * (5 - 0) + 0);
-
   //se concatena el mensaje y se almacena en la variable
   let escusa =
-    lengujues.Ingles.who[who1] +
-    " " +
-    lengujues.Ingles.action[action1] +
-    " " +
-    lengujues.Ingles.what[what1] +
-    " " +
-    lengujues.Ingles.when[when1];
+    who[who1] + " " + action[action1] + " " + what[what1] + " " + when[when1];
 
-  return escusa;
+  return (document.getElementById("excuse").innerHTML = escusa);
 };
-
-function render() {
-  const resultado = cambio();
-  return (document.getElementById("excuse").innerHTML = resultado);
-}
+s
